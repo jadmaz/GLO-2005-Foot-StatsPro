a964f599acb94ce7258306b0baecb5c6248dfef7
jadmaz <jamaz6@ulaval.ca>
2024-03-31 02:34:44 -0400

    -F Refactor

--- FootStatsPro/vite.config.js
+++ FootStatsPro/vite.config.js
@@ -8,9 +8,6 @@ export default defineConfig({
   plugins: [
     vue(),
   ],
-  server: {
-    port: 5174 // Spécifie le port sur lequel le serveur Vite doit écouter
-  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
